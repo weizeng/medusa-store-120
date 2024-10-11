74596b3112e9af1a1a680b3db7895c3e924a4f9b
leo <leo.wei.badger@gmail.com>
2024-10-11 13:59:38 +0800

    s3 ey

--- medusa-config.js
+++ medusa-config.js
@@ -62,7 +62,7 @@ const plugins = [
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: true,
-      path: "/admin",
+      // path: "/admin",
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
       },
